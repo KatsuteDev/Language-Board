403cccb6654d5fa4dafc0e21d694fd879b528c9e
Katsute <58778985+Katsute@users.noreply.github.com>
2023-07-18 17:54:26 -0400

    update deployment

---
+++ forge.config.js
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2023 Katsute <https://github.com/Katsute>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+const path = require("path");
+
+module.exports = {
+    packagerConfig: {
+        name: "Language Board",
+        icon: "assets/icon.ico",
+        appCopyright: "Copyright © Katsute 2023",
+        ignore: [
+            "^/\\.devcontainer$",
+            "^/\\.github$",
+            "^/src$",
+            "^/\\.gitignore$",
+            "^/\\w*\\.(js|png|gif)$",
+            "^/tsconfig.json$"
+        ]
+    },
+    makers: [
+        {
+            name: "@electron-forge/maker-squirrel",
+            config: {
+                name: "Language-Board",
+                copyright: "Copyright © Katsute 2023",
+                iconUrl: "https://github.com/KatsuteDev/Language-Board/blob/main/assets/icon.ico",
+                loadingGif: "assets/setup.gif",
+                setupExe: "Language Board Setup.exe",
+                setupIcon: "assets/icon.ico",
+                skipUpdateIcon: true
+            }
+        },
+        {
+            name: "@electron-forge/maker-wix",
+            config: {
+                name: "Language Board",
+                manufacturer: "Katsute",
+                icon: "assets/icon.ico",
+                ui: {
+                    chooseDirectory: true,
+                    images: {
+                        background: path.join(__dirname, "assets", "setup.png"),
+                        banner: path.join(__dirname, "assets", "setup.png")
+                    }
+                }
+            }
+        },
+        {
+            name: "@electron-forge/maker-zip"
+        }
+    ]
+}
---
+++ post.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2023 Katsute <https://github.com/Katsute>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+const fs = require("fs");
+const path = require("path");
+const pkg = require("./package.json");
+
+const make = path.join(__dirname, "out", "make");
+
+{
+    const dir = path.join(make, "squirrel.windows", "ia32");
+    fs.renameSync(path.join(dir, `Language Board Setup.exe`), path.join(dir, `Language-Board-Setup-${pkg.version}-Windows-x86-32.exe`));
+}
+{
+    const dir = path.join(make, "squirrel.windows", "x64");
+    fs.renameSync(path.join(dir, `Language Board Setup.exe`), path.join(dir, `Language-Board-Setup-${pkg.version}-Windows-x64.exe`));
+}
+
+{
+    const dir = path.join(make, "wix", "ia32");
+    fs.renameSync(path.join(dir, `Language Board.msi`), path.join(dir, `Language-Board-Setup-${pkg.version}-Windows-x86-32.msi`));
+}
+{
+    const dir = path.join(make, "wix", "x64");
+    fs.renameSync(path.join(dir, `Language Board.msi`), path.join(dir, `Language-Board-Setup-${pkg.version}-Windows-x64.msi`));
+}
+
+{
+    const dir = path.join(make, "zip", "win32", "ia32");
+    fs.renameSync(path.join(dir, `Language Board-win32-ia32-${pkg.version}.zip`), path.join(dir, `Language-Board-${pkg.version}-Windows-x86-32.zip`));
+}
+{
+    const dir = path.join(make, "zip", "win32", "x64");
+    fs.renameSync(path.join(dir, `Language Board-win32-x64-${pkg.version}.zip`), path.join(dir, `Language-Board-${pkg.version}-Windows-x64.zip`));
+}
